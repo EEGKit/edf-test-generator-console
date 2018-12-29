2f35674cf8ec9f834012ff0b10656a00646dfa9f
Teuniz <teuniz@gmail.com>
2018-12-29 22:26:43 +0100

    Work in progress.

--- main.c
+++ main.c
@@ -17,6 +17,13 @@
 #include "edflib.h"
 
 
+#define FILETYPE_EDF  0
+#define FILETYPE_BDF  1
+
+#define WAVE_SINE     0
+#define WAVE_SQUARE   1
+
+
 
 int main(int argc, char **argv)
 {
@@ -26,33 +33,38 @@ int main(int argc, char **argv)
       duration=1,
       sf=1,
       digmax=0,
-      digmin=0;
+      digmin=0,
+      waveform;
 
   double *buf=NULL,
          w=1,
+         q=1,
          sine_1=0,
+         square_1=0,
          signalfreq=1,
          physmax=1200,
          physmin=-1200,
-         peakamp=1000;
+         peakamp=1000,
+         dutycycle=50,
+         ftmp;
 
   char physdim[32]="uV",
        str[128]="";
 
-  if((argc != 5) && (argc != 9) && (argc != 11))
+  if((argc != 7) && (argc != 11) && (argc != 13))
   {
-    printf("\nusage: edf_generator <filetype edf or bdf> <duration in seconds> <sample frequency> <signal frequency>\n"
-           " [<physical max> <physical min> <peak amplitude> <physical dimension> [<digital max> <digital min>]]\n"
-           "\nexample: edf_generator edf 10 1000 1\n"
-           "\nexample: edf_generator edf 10 113.67 1 3200 -3200 100 uV\n"
-           "\nexample: edf_generator bdf 10 1000 1.5 1000 -1000 300 mV 1048575 -1048576\n\n");
+    printf("\nusage: edf_generator <filetype edf or bdf> <duration in seconds> <sample frequency> <signal frequency Hz> <waveform sine or square> <dutycycle %%>"
+           " [<physical max> <physical min> <peak amplitude> <physical dimension> [<digital max> <digital min>]]"
+           "\nexample: edf_generator edf 10 1000 1 sine 50\n"
+           "\nexample: edf_generator edf 10 113.67 1 square 50 3200 -3200 100 uV\n"
+           "\nexample: edf_generator bdf 10 1000 1.5 square 10.5 1000 -1000 300 mV 1048575 -1048576\n\n");
 
     return EXIT_FAILURE;
   }
 
   if(!strcmp(argv[1], "edf"))
   {
-    filetype = 0;
+    filetype = FILETYPE_EDF;
 
     digmax = 32767;
 
@@ -60,7 +72,7 @@ int main(int argc, char **argv)
   }
   else if(!strcmp(argv[1], "bdf"))
     {
-      filetype = 1;
+      filetype = FILETYPE_BDF;
 
       digmax = 8388607;
 
@@ -97,15 +109,38 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  if((argc == 9) || (argc == 11))
+  if(!strcmp(argv[5], "sine"))
   {
-    physmax = atof(argv[5]);
+    waveform = WAVE_SINE;
+  }
+  else if(!strcmp(argv[5], "square"))
+    {
+      waveform = WAVE_SQUARE;
+    }
+    else
+    {
+      printf("error: invalid waveform %s\n", argv[1]);
 
-    physmin = atof(argv[6]);
+      return EXIT_FAILURE;
+    }
 
-    peakamp = atof(argv[7]);
+  dutycycle = atof(argv[6]);
+  if((dutycycle < 0.0999) || (dutycycle > 99.9001))
+  {
+    printf("error: invalid duty cycle %f %%\n", dutycycle);
 
-    strncpy(physdim, argv[8], 16);
+    return EXIT_FAILURE;
+  }
+
+  if((argc == 11) || (argc == 13))
+  {
+    physmax = atof(argv[7]);
+
+    physmin = atof(argv[8]);
+
+    peakamp = atof(argv[9]);
+
+    strncpy(physdim, argv[10], 16);
 
     physdim[16] = 0;
 
@@ -138,13 +173,13 @@ int main(int argc, char **argv)
     }
   }
 
-  if(argc == 11)
+  if(argc == 13)
   {
-    digmax = atoi(argv[9]);
+    digmax = atoi(argv[11]);
 
-    digmin = atoi(argv[10]);
+    digmin = atoi(argv[12]);
 
-    if(filetype)
+    if(filetype == FILETYPE_BDF)
     {
       if(digmax > 8388607)
       {
@@ -160,22 +195,22 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
       }
     }
-    else
-    {
-      if(digmax > 32767)
+    else if(filetype == FILETYPE_EDF)
       {
-        printf("error: digital max must be <= 32767\n");
+        if(digmax > 32767)
+        {
+          printf("error: digital max must be <= 32767\n");
 
-        return EXIT_FAILURE;
+          return EXIT_FAILURE;
+        }
+
+        if(digmin < -32768)
+        {
+          printf("error: digital min must be >= -32768\n");
+
+          return EXIT_FAILURE;
+        }
       }
-
-      if(digmin < -32768)
-      {
-        printf("error: digital min must be >= -32768\n");
-
-        return EXIT_FAILURE;
-      }
-    }
 
     if(digmin >= digmax)
     {
@@ -193,14 +228,14 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  if(filetype)
+  if(filetype == FILETYPE_BDF)
   {
     hdl = edfopen_file_writeonly("edf_generator.bdf", EDFLIB_FILETYPE_BDFPLUS, 1);
   }
-  else
-  {
-    hdl = edfopen_file_writeonly("edf_generator.edf", EDFLIB_FILETYPE_EDFPLUS, 1);
-  }
+  else if(filetype == FILETYPE_EDF)
+    {
+      hdl = edfopen_file_writeonly("edf_generator.edf", EDFLIB_FILETYPE_EDFPLUS, 1);
+    }
 
   if(hdl<0)
   {
@@ -252,7 +287,14 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  snprintf(str, 18, "sine %.2fHz", signalfreq);
+  if(waveform == WAVE_SINE)
+  {
+    snprintf(str, 18, "sine %.2fHz", signalfreq);
+  }
+  else if(waveform == WAVE_SQUARE)
+    {
+      snprintf(str, 18, "square %.2fHz", signalfreq);
+    }
 
   if(edf_set_label(hdl, 0, str))
   {
@@ -263,16 +305,42 @@ int main(int argc, char **argv)
 
   sine_1 = 0.0;
 
+  square_1 = 0.0;
+
   w = M_PI * 2.0;
+
+  q = 1.0 / sf;
 
   w /= (sf / signalfreq);
 
   for(j=0; j<duration; j++)
   {
-    for(i=0; i<sf; i++)
+    if(waveform == WAVE_SINE)
     {
-      sine_1 += w;
-      buf[i] = sin(sine_1) * peakamp;
+      for(i=0; i<sf; i++)
+      {
+        sine_1 += w;
+
+        buf[i] = sin(sine_1) * peakamp;
+      }
+    }
+    else if(waveform == WAVE_SQUARE)
+    {
+      for(i=0; i<sf; i++)
+      {
+        square_1 += q;
+
+        ftmp = fmod(square_1, 1.0 / signalfreq);
+
+        if((ftmp * signalfreq) < (dutycycle / 100.0))
+        {
+          buf[i] = peakamp;
+        }
+        else
+        {
+          buf[i] = -peakamp;
+        }
+      }
     }
 
     if(edfwrite_physical_samples(hdl, buf))
